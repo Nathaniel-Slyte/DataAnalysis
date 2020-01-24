cb2a0d95c68ecaa12dfece28d4a088ba2efe87ec
Nathaniel-Slyte <parilusb@hotmail.fr>
2020-01-24 16:54:28 +0100

    flemme et seum

--- data_handle.py
+++ data_handle.py
@@ -1,1 +1,21 @@
-o
+"""data_handle.py
+
+this script is designed to select revelant features from the dataset and create
+a simplified sample of data for classification model training purpose
+"""
+
+import os, csv, json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from timeit import default_timer as timer
+from sklearn.decomposition import PCA
+from joblib import dump, load
+from scipy import stats
+
+
+
+
+if __name__ == '__main__' :
